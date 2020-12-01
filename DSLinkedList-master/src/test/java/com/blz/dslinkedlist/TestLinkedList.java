36e27690dcd3d22a3041b46be1b4632fc2db8630
shubhamsonawane830 <shubhamsonawane830@gmail.com>
2020-12-01 09:55:37 +0530

    Added: Delete Last Element

--- DSLinkedList-master/src/test/java/com/blz/dslinkedlist/TestLinkedList.java
+++ DSLinkedList-master/src/test/java/com/blz/dslinkedlist/TestLinkedList.java
@@ -73,4 +73,18 @@ public class TestLinkedList {
 		boolean result = MyLinkedList.head.equals(mySecondNode) && MyLinkedList.tail.equals(myThirdNode);
 		Assert.assertTrue(result);
 	}
+	
+	@Test
+	public void given3NumbersWhenAddedtoLinkedListAndDeleteLastElement() {
+		MyNode<Integer> myFirstNode = new MyNode<Integer>(56);
+		MyNode<Integer> mySecondNode = new MyNode<Integer>(30);
+		MyNode<Integer> myThirdNode = new MyNode<Integer>(70);
+		MyLinkedList lst = new MyLinkedList();
+		lst.add(myFirstNode);
+		lst.append(mySecondNode);
+		lst.append(myThirdNode);
+		lst.popLast();
+		boolean result = MyLinkedList.head.equals(myFirstNode) && MyLinkedList.tail.equals(mySecondNode);
+		Assert.assertTrue(result);
+	}
 }
