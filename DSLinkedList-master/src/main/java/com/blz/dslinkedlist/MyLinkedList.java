0a05799be9f8aa3298f757138535bd8d64192baf
shubhamsonawane830 <shubhamsonawane830@gmail.com>
2020-12-01 08:28:59 +0530

    Added: Element at top of linkedlist

--- DSLinkedList-master/src/main/java/com/blz/dslinkedlist/MyLinkedList.java
+++ DSLinkedList-master/src/main/java/com/blz/dslinkedlist/MyLinkedList.java
@@ -6,7 +6,7 @@ public class MyLinkedList<K> {
 
 	public MyLinkedList() {
 		this.head = null;
-		this.tail = null ;
+		this.tail = null;
 	}
 
 	public void add(INode<K> newNode) {
@@ -19,5 +19,4 @@ public class MyLinkedList<K> {
 			this.head.setNext(tempNode);
 		}
 	}
-
 }
--- DSLinkedList-master/src/test/java/com/blz/dslinkedlist/TestLinkedList.java
+++ DSLinkedList-master/src/test/java/com/blz/dslinkedlist/TestLinkedList.java
@@ -17,4 +17,18 @@ public class TestLinkedList {
 		boolean result = myFirstNode.getNext().equals(mySecondNode) && mySecondNode.getNext().equals(myThirdNode);
 		Assert.assertTrue(result);
 	}
+	
+	@Test
+	public void given3NumbersWhenAddedToLinkedListShouldBeAddedAtTheTop() {
+		MyNode<Integer> myFirstNode = new MyNode<Integer>(70);
+		MyNode<Integer> mySecondNode = new MyNode<Integer>(30);
+		MyNode<Integer> myThirdNode = new MyNode<Integer>(56);
+		MyLinkedList lst = new MyLinkedList();
+		lst.add(myFirstNode);
+		lst.add(mySecondNode);
+		lst.add(myThirdNode);
+		boolean result = MyLinkedList.head.equals(myThirdNode) && MyLinkedList.head.getNext().equals(mySecondNode)
+				&& MyLinkedList.tail.equals(myFirstNode);
+		Assert.assertTrue(result);
+	}
 }
